3722493591015b27441ca3443904c9f33487c8dc
yoshi0624 <136308862+yoshi0624@users.noreply.github.com>
2023-06-25 22:46:28 +0900

    Enemyprefab
    Enemyprefabをつけ加えています

--- Assets/Script/ScoreCount.cs
+++ Assets/Script/ScoreCount.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreCount : MonoBehaviour
+public class Scorecount : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
     private float score;
